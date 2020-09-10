3ad5a912aa4879c9b02cf14ef85adb8a29880237
10Dozen <steel.mjr@gmail.com>
2020-09-10 20:04:05 +0300

    MU fixes

--- MissionDownloader/MissionDownloader.py
+++ MissionDownloader/MissionDownloader.py
@@ -1,51 +1,72 @@
 url = input("GitHub repo URL:")
 mode = int(input("Select mode: [1] - Download, [2] - Download & pack\n"))
+steps = "6"
 
 import requests
 import re
 import os
+import shutil
+import sys
 
 def DownalodAndUnzipFiles(url):
-    print("Getting Readme.md...")
+    print("[1/" + steps + "] Getting Readme.md...")
 
     urlReadme = "https://raw.githubusercontent.com/" + url.rsplit('/',2)[1] + "/" + url.rsplit('/',2)[2] + "/master/README.md"
 
     readmeRequest = requests.get(urlReadme, allow_redirects=True)
     readmeText = readmeRequest.text
 
+    version = "1A"    
     versionRXP = re.compile('(Version: (\d[A-Za-z]))')
-    version = versionRXP.search(readmeText)[2]
+    versionSearched = versionRXP.search(readmeText)
+    if versionSearched:
+        version = versionSearched[2]
 
     print("Version: " + version)
 
-    print("Getting mission archive...")
+    print("[2/" + steps + "] Getting mission archive...")
 
     urlZip = url + "/archive/master.zip"
 
     filename = url.rsplit('/',1)[1] + "-master"
     zipFilename = filename + "-master.zip"
     zipRequest = requests.get(urlZip, allow_redirects=True)
+    
+    if os.path.exists(zipFilename):
+        os.remove(zipFilename)
+    
     open(zipFilename, "wb").write(zipRequest.content)
 
     print("Mission archive downloaded - " + zipFilename)
 
-    print("Unzipping archive...")
+    print("[3/" + steps + "] Unzipping archive...")
 
     from zipfile import ZipFile
     with ZipFile(zipFilename,'r') as zipObj:
         zipObj.extractall()
         
     os.remove(zipFilename)
-        
+
     print("Unzipped to " + filename)
 
-    print("Renaming folder...")
-
-    rxp = re.compile('(.*)((\.)(.*)(-master))')
-    filenameParts = (rxp.findall(filename))[0]
-    filenameRenamed = filenameParts[0] + "_" + version + "." + filenameParts[3]
+    print("[4/" + steps + "] Renaming folder...")
+    
+    if (re.search(r'(\d[a-zA-Z].)', filename, re.I)):
+        filenameRenamed = filename.replace("-master","")
+    else:
+        rxp = re.compile('(.*)((\.)(.*)(-master))')
+        filenameParts = (rxp.findall(filename))[0]
+        filenameRenamed = filenameParts[0] + "_" + version + "." + filenameParts[3]
+    
+    if os.path.exists(filenameRenamed):
+        print("" + filenameRenamed + " already exists!")
+        dialog = input("\nRemove existing directory? [Y] [N] -> ")
+        if dialog.lower() == 'y':
+            shutil.rmtree(filenameRenamed)
+        else:
+            sys.exit("Directory already exists")
+   
     os.rename(filename, filenameRenamed)
-
     print("Renamed to " + filenameRenamed)
     
     return filenameRenamed
@@ -55,38 +76,61 @@ def PackMission(filename):
     
     CleanMissionFiles(filename)
     
-    print("Packing...")
+    print("[6/" + steps + "] Packing...")
     import subprocess
     subprocess.run(["MakePbo","-N", os.path.abspath(filename)])
     print("Packed!")
 
 
 def CleanMissionFiles(filename):
-    print("Cleaning mission files from " + filename + "...")
+    print("[5/" + steps + "] Cleaning mission files from " + filename + "...")
     
-    """
-    - Remove Helpers at:
-        filename\init3DEN.sqf
-        filename\README.md
-        filename\tSF_FileSweeper.bat
-        filename\dzn_tSFramework\tS_SettingsOverview.html
-        filename\dzn_tSFramework\3DEN\*
-        filename\dzn_tSFramework\Modules\Briefing\BriefingHelper\*
-        filename\dzn_tSFramework\Modules\MissionConditions\EndingsHelper\*
-        filename\dzn_dynai\tools
-        
-    - Read filename/dzn_tSFramework/dzn_tSFramework_Init.sqf
-    - Find all 'tSF_module_<MODULE_NAME> = <BOOL>' and move in pairs
-    - For each pair -- if <BOOL> = false --> remove filename/dzn_tSFramework/Modules/<MODULE_NAME> folder 
- 
-    """
-    pass
+    ### Remove files 
+    files = [
+        os.path.join(filename, "init3DEN.sqf"),
+        os.path.join(filename, "README.md"),
+        os.path.join(filename, "tSF_FileSweeper.bat"),
+        os.path.join(filename, "tSF_FS_log.txt"),
+        os.path.join(filename, ".gitattributes"),
+        os.path.join(filename, "dzn_tSFramework\\tS_SettingsOverview.html")
+    ]
+    dirs = [
+        os.path.join(filename, "dzn_tSFramework\\3DEN\\"),
+        os.path.join(filename, "dzn_tSFramework\\Modules\\Briefing\\BriefingHelper\\"),
+        os.path.join(filename, "dzn_tSFramework\\Modules\\MissionConditions\\EndingsHelper\\"),
+        os.path.join(filename, "dzn_dynai\\tools\\")
+    ]
+    
+    for f in files:
+        if os.path.exists(f):
+            print("    Deleting " + f)
+            os.remove(f)
+    
+    for d in dirs:
+        if os.path.exists(d):
+            print("    Deleting directory " + d)
+            shutil.rmtree(d)
+    
+    print("\nRemoving unused tSF Modules...")
+   
+    tSDir = os.path.join(filename, "dzn_tSFramework\\Modules\\")
+    tSFFile = open(os.path.join(filename, "dzn_tSFramework\\dzn_tSFramework_Init.sqf"), "r")
+    for line in tSFFile:
+        r = re.search(r'(tSF_module_)([a-zA-Z\_]*)([\s=]+)(false|true)', line, re.I)
+        if r:
+            moduleDir = r[2]
+            moduleEnabled = r[4].lower() == 'true'
+            modulePath = os.path.join(tSDir, moduleDir)
+            
+            if not moduleEnabled and os.path.exists(modulePath):
+                print("    Module " + moduleDir + " is disabled: removing " + os.path.join(tSDir, moduleDir))
+                shutil.rmtree(modulePath)
+    
+    tSFFile.close()
+
 
 ### Start
-if mode == 1:
-    print("Executing mode - Download")
-elif mode == 2:
-    print("Executing mode - Download & Pack to PBO")
+print("Execution mode - Download") if mode == 1 else print("Execution mode - Download & Pack to PBO")
 
 filename = DownalodAndUnzipFiles(url)
 if mode == 2:
